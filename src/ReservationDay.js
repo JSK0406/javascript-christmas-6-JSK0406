0dc6a4e503279e75e906740aec62c95219349d01
JiSeong Kim <12180543@inha.edu>
2023-11-11 00:15:47 +0900

    feat(ReservationDay): add check christmasEvent
    상수의 calendar를 통해 현재 날짜가 크리스마스 이벤트를 진행하고 있는지 확인

--- src/ReservationDay.js
+++ src/ReservationDay.js
@@ -1,4 +1,5 @@
 import ReservationDayValidator from './validator/ReservationDayValidator.js';
+import calender from './constants/calendar.js';
 
 class ReservationDay {
   #day;
@@ -6,6 +7,10 @@ class ReservationDay {
   constructor(day) {
     ReservationDayValidator.validate(day);
     this.#day = Number(day);
+  }
+
+  isChristmasEvent() {
+    return calender.CHRISTMAS_D_DAY.includes(this.#day);
   }
 }
 
