4217b9a337dc6b24ecd02e31c86d98971e774e95
JiSeong Kim <12180543@inha.edu>
2023-11-11 02:55:47 +0900

    feat(Event): add calculating discount methods
    - 평일 할인 계산 메소드
    - 주말 할인 계산 메소드
    - 특별 할인 계산 메소드

--- src/Event.js
+++ src/Event.js
@@ -11,6 +11,18 @@ class Event {
   calculateChristmasDiscount(day) {
     return restaurantRule.CHRISTMAS_EVENT_LEAST_DISCOUNT + restaurantRule.CHRISTMAS_EVENT_DISCOUNT_INCREMENT_BY_DAY * (day - 1);
   }
+
+  calculateWeekdayDiscount(dessertCount) {
+    return dessertCount * restaurantRule.WEEKDAY_DISCOUNT_BY_COUNT;
+  }
+
+  calculateWeekendDiscount(mainCount) {
+    return mainCount * restaurantRule.WEEKEND_DISCOUNT_BY_COUNT;
+  }
+  
+  calculateSpecialDiscount() {
+    return restaurantRule.SPECIAL_DISCOUNT;
+  }
 }
 
 export default Event;
