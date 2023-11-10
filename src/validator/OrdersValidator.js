59e8734ee8459695ac9add9bef26a258cee88498
JiSeong Kim <12180543@inha.edu>
2023-11-11 01:59:21 +0900

    feat(validator): implement OrdersValidator
    - 한 메뉴의 주문 개수 검증
    - 주문 개수가 문자가 아닌 숫자인지 검증
    - 주문 개수가 비정상적인 입력이 아닌지 검증
    - 주문 개수가 자연수인지 검증
    - 주문 음식이 menu안에 있는지 검증

---
+++ src/validator/OrdersValidator.js
@@ -0,0 +1,62 @@
+import errorMessage from '../constants/errorMessage.js';
+import menu from '../constants/menu.js';
+
+class OrdersValidator {
+  static validate(orders) {
+    const ordersValidator = new OrdersValidator();
+    const ordersError = new Error(errorMessage.ORDERS);
+    orders = orders.split(',');
+    ordersValidator.#validateCountRange(orders, ordersError);
+    ordersValidator.#validateInMenuList(orders, ordersError);
+    ordersValidator.#validateIsCountNaturalNumber(orders, ordersError);
+    ordersValidator.#validateIsCountNumber(orders, ordersError);
+    ordersValidator.#validateIsNotUnusualCount(orders, ordersError);
+  }
+ 
+  #validateInMenuList(orders, ordersError) {
+    orders.forEach((order) => {
+      const menuName = order.split('-')[0];
+      if (menu[menuName] === undefined) {
+        throw ordersError;
+      }
+    });
+  }
+
+  #validateCountRange(orders, ordersError) {
+    orders.forEach((order) => {
+      const count = Number(order.split('-')[1]);
+      if (count < 1 || count > 20) {
+        throw ordersError;
+      }
+    });
+  }
+
+  #validateIsCountNumber(orders, ordersError) {
+    orders.forEach((order) => {
+      const count = Number(order.split('-')[1]);
+      if (Number.isNaN(count)) {
+        throw ordersError;
+      }
+    });
+  }
+
+  #validateIsNotUnusualCount(orders, ordersError) {
+    orders.forEach((order) => {
+      const countString = order.split('-')[1];
+      if (countString !== Number(countString).toString()) {
+        throw ordersError;
+      }
+    });
+  }
+
+  #validateIsCountNaturalNumber(orders, ordersError) {
+    orders.forEach((order) => {
+      const count = Number(order.split('-')[1]);
+      if (!Number.isInteger(count)) {
+        throw ordersError;
+      }
+    });
+  }
+}
+
+export default OrdersValidator;
