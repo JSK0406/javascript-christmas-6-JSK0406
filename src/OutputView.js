b1ed71fbbcb1995e2b3dc3c4507634541877907c
JiSeong Kim <12180543@inha.edu>
2023-11-11 23:58:15 +0900

    feat(OutputView): implement OutputView
    - 사용자에게 보이는 출력에 대한 print함수들을 정의

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,9 +1,75 @@
+import { OUTPUT_VIEW_MESSAGE } from './constants/viewMessage.js';
+import { Console } from '@woowacourse/mission-utils';
+import Formatting from './utils/Formatting.js';
+
 const OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
+  printNewLine() {
+    Console.print('');
+  },
+  printProgramInit() {
+    Console.print(OUTPUT_VIEW_MESSAGE.programInit);
+  },
+  printIntroduce(day) {
+    Console.print(OUTPUT_VIEW_MESSAGE.introduceFn(day));
+  },
+  printOrderedMenuInit() {
+    Console.print(OUTPUT_VIEW_MESSAGE.orderedMenusInit);
+  },
+  printOrderedMenu(orders) {
+    orders.forEach(({ menuName, orderedCount }) => {
+      console.print(OUTPUT_VIEW_MESSAGE.orderedMenuFn(menuName, orderedCount));
+    });
+  },
+  printBeforeDiscountInit() {
+    Console.print(OUTPUT_VIEW_MESSAGE.beforeDiscountInit);
+  },
+  printBeforeDiscount(price) {
+    Console.print(OUTPUT_VIEW_MESSAGE.afterDiscountFn(Formatting.insertCommasByThousandUnits(price)))
+  },
+  printGiveawayInit() {
+    Console.print(OUTPUT_VIEW_MESSAGE.giveawayInit);
+  },
+  printGiveaway() {
+    Console.print(OUTPUT_VIEW_MESSAGE.giveaway);
+  },
+  printDiscountHistoryInit() {
+    Console.print(OUTPUT_VIEW_MESSAGE.discountsInit);
+  },
+  printDiscountHistory(discountHistory) {
+    if (discountHistory.christmas > 0) {
+      Console.print(OUTPUT_VIEW_MESSAGE.christmasDiscountFn(Formatting.insertCommasByThousandUnits(discountHistory.christmas)))
     }
-    // ...
-}
+    if (discountHistory.weekday > 0) {
+      Console.print(OUTPUT_VIEW_MESSAGE.weekendDiscountFn(Formatting.insertCommasByThousandUnits(discountHistory.weekday)))
+    }
+    if (discountHistory.weekend > 0) {
+      Console.print(OUTPUT_VIEW_MESSAGE.weekendDiscountFn(Formatting.insertCommasByThousandUnits(discountHistory.weekend)))
+    }
+    if (discountHistory.special > 0) {
+      Console.print(OUTPUT_VIEW_MESSAGE.specialDiscountFn(Formatting.insertCommasByThousandUnits(discountHistory.special)))
+    }
+    if (discountHistory.giveaway > 0) {
+      Console.print(OUTPUT_VIEW_MESSAGE.christmasDiscountFn(Formatting.insertCommasByThousandUnits(discountHistory.giveaway)))
+    }
+  },
+  printTotalDiscountInit() {
+    Console.print(OUTPUT_VIEW_MESSAGE.totalDiscountInit);
+  },
+  printTotalDiscount(price) {
+    Console.print(OUTPUT_VIEW_MESSAGE.totalDiscountFn(Formatting.insertCommasByThousandUnits(price)));
+  },
+  printAfterDiscountInit() {
+    Console.print(OUTPUT_VIEW_MESSAGE.afterDiscountInit);
+  },
+  printAfterDiscount(price) {
+    Console.print(OUTPUT_VIEW_MESSAGE.afterDiscountFn(Formatting.insertCommasByThousandUnits(price)));
+  },
+  printBadgeInit() {
+    Console.print(OUTPUT_VIEW_MESSAGE.badgeInit);
+  },
+  printBadge(badge) {
+    Console.print(badge)
+  }
+};
 
 export default OutputView;
